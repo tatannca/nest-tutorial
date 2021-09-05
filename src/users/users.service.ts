9f986f6979d6541f6d08a74d4bead9fa881dac08
tateno <gd28.c.tateno@gmail.com>
2021-09-05 21:20:55 +0900

    add users add package, class-transformer, class-validator update tsconfig

---
+++ src/users/users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create.user.dto';
+
+@Injectable()
+export class UsersService {
+  users: CreateUserDto[] = [];
+
+  create(user: CreateUserDto) {
+    this.users.push(user);
+  }
+
+  findAll() {
+    return this.users;
+  }
+}
